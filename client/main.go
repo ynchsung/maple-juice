e3e2b2b66b2ab5b55c3876e13b5eb23210572a0b
ycsung2 <ycsung2@illinois.edu>
2019-09-14 23:47:23 -0500

    Change client print code

--- client/main.go
+++ client/main.go
@@ -15,7 +15,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	args := &common.ArgGrep{[]string{"machine.i.log"}, "712.*"}
+	args := &common.ArgGrep{[]string{"vm1.log", "vm2.log", "vm3.log", "vm4.log", "vm5.log", "vm6.log", "vm7.log", "vm8.log", "vm9.log", "vm10.log"}, "^[0-9]*[a-z]{5}"}
 	var reply common.ReplyGrepList
 	err = client.Call("RpcClient.GrepLogFile", args, &reply)
 	if err == nil {
@@ -27,12 +27,14 @@ func main() {
 
 			fmt.Printf("Host %v got %v lines\n", replyGrep.Host, replyGrep.LineCount)
 			for _, file := range replyGrep.Files {
-				fmt.Printf("\t%v\n", file.Path)
-				fmt.Printf("\t===============\n")
-				for _, line := range file.Lines {
-					fmt.Printf("\tLine %v: %v\n", line.LineNum, line.LineStr)
-				}
-				fmt.Printf("\n")
+				fmt.Printf("\t%v has %v lines\n", file.Path, len(file.Lines))
+				/*
+					fmt.Printf("\t--\n")
+					for _, line := range file.Lines {
+						fmt.Printf("\tLine %v: %v\n", line.LineNum, line.LineStr)
+					}
+					fmt.Printf("\n\t========================\n\n")
+				*/
 			}
 		}
 	}
